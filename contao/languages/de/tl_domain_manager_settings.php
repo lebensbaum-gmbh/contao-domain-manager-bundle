57c5397242945461864b25af0c4c4b03b36828c8
Jörg Zülich <58428037+Akranor@users.noreply.github.com>
2026-08-19 15:25:29 +0200

    Add health setting translations

--- contao/languages/de/tl_domain_manager_settings.php
+++ contao/languages/de/tl_domain_manager_settings.php
@@ -3,11 +3,16 @@
 declare(strict_types=1);
 
 $GLOBALS['TL_LANG']['tl_domain_manager_settings']['access_legend'] = 'Frontend-Zugriff';
+$GLOBALS['TL_LANG']['tl_domain_manager_settings']['health_legend'] = 'Status & Warnungen';
 $GLOBALS['TL_LANG']['tl_domain_manager_settings']['links_legend'] = 'Externe Dienste';
 
 $GLOBALS['TL_LANG']['tl_domain_manager_settings']['sync_member_groups'] = [
     'Berechtigte Mitgliedergruppen',
     'Nur angemeldete Frontend-Mitglieder aus mindestens einer dieser Gruppen dürfen „Systemdaten aktualisieren“ ausführen. Ohne Auswahl ist die Synchronisation im Frontend deaktiviert.',
+];
+$GLOBALS['TL_LANG']['tl_domain_manager_settings']['stale_sync_days'] = [
+    'Synchronisationswarnung nach Tagen',
+    'Zeigt einen Hinweis an, wenn die letzte erfolgreiche Synchronisation länger als diese Anzahl von Tagen zurückliegt. Standard: 30 Tage.',
 ];
 $GLOBALS['TL_LANG']['tl_domain_manager_settings']['trakked_url'] = [
     'Trakked-URL',
