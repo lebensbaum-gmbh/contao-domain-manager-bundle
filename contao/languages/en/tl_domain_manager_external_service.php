67619f355a3f33650928c32e416030edba4c8cd3
Jörg Zülich <58428037+Akranor@users.noreply.github.com>
2026-08-24 13:07:12 +0200

    Add English external service translations

---
+++ contao/languages/en/tl_domain_manager_external_service.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_LANG']['tl_domain_manager_external_service']['service_legend'] = 'External service';
+
+$GLOBALS['TL_LANG']['tl_domain_manager_external_service']['name'] = [
+    'Label',
+    'Display name of the external service, e.g. Trakked, GitHub or hosting.',
+];
+$GLOBALS['TL_LANG']['tl_domain_manager_external_service']['url'] = [
+    'URL',
+    'Target URL of the external service. The link is shown in the domain overview.',
+];
+
+$GLOBALS['TL_LANG']['tl_domain_manager_external_service']['new'] = ['New service', 'Create a new external service.'];
+$GLOBALS['TL_LANG']['tl_domain_manager_external_service']['edit'] = ['Edit', 'Edit external service ID %s.'];
+$GLOBALS['TL_LANG']['tl_domain_manager_external_service']['copy'] = ['Duplicate', 'Duplicate external service ID %s.'];
+$GLOBALS['TL_LANG']['tl_domain_manager_external_service']['delete'] = ['Delete', 'Delete external service ID %s.'];
+$GLOBALS['TL_LANG']['tl_domain_manager_external_service']['show'] = ['Details', 'Show external service ID %s.'];
