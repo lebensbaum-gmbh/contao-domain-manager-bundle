6903ef4315d6b4c0fe0b1682810930e754c5ed41
Jörg Zülich <58428037+Akranor@users.noreply.github.com>
2026-09-16 14:06:06 +0200

    Register Domain Manager navigation module palette

---
+++ contao/dca/tl_module.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_DCA']['tl_module']['palettes']['domain_manager_navigation'] = '
+    {title_legend},name,type;
+    {template_legend:hide},customTpl;
+    {protected_legend:hide},protected;
+    {expert_legend:hide},guests,cssID
+';
